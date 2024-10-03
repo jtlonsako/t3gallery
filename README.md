4e86be78b3fa0aac2d99dd98652e50637bc50a00
jtlonsako <jtlonsako.work@gmail.com>
2024-10-02 17:31:44 -0700

    Linked database to UI

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 - [x] Scaffold basic ui with mock data
 - [x] Tidy up build process
 - [x] Actually setup a database
-- [ ] Attach database to UI
+- [x] Attach database to UI
 - [ ] Add authentication
 - [ ] Add image upload
 - [ ] Error management
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ const mockUrls = [
   "https://utfs.io/f/a9tNkK5IuJOnHZ6EGTmeYrM7WscaVpHNQqCL6XGbEl03KIOU",
   "https://utfs.io/f/a9tNkK5IuJOnWwEubk14liMesw2j6yDodtqC3INRPku7az8K",
   "https://utfs.io/f/a9tNkK5IuJOnsskUapQSQ6Oh0q8dT9utzCeaAUwxEJRBLfnK"
-
 ];
 
 const mockImages = mockUrls.map((url, index) => ({
@@ -16,15 +15,17 @@ const mockImages = mockUrls.map((url, index) => ({
 
 export default async function HomePage() {
 
-  const posts = await db.query.posts.findMany();
+  const images = await db.query.images.findMany({
+    orderBy:(model, { desc }) => desc(model.id)
+  });
 
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {posts.map((post) => (<div key={post.id}>{post.name}</div>))}
-        {[...mockImages, ...mockImages, ...mockImages, ...mockImages].map((image, index) => (
-          <div key={image.id + '-' + index} className="w-48 p-4">
+        {images.map((image, index) => (
+          <div key={image.id + '-' + index} className="w-48 p-4 flex flex-col">
             <img src={image.url} />
+            <div>{image.name}</div>
           </div>
         ))
       
