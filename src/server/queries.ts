6c80e5ced111e2724475f2968e719a26ee3e0d7c
jtlonsako <jtlonsako.work@gmail.com>
2024-10-03 17:25:31 -0700

    added analytics for deletions

--- src/server/queries.ts
+++ src/server/queries.ts
@@ -4,6 +4,7 @@ import { auth } from "@clerk/nextjs/server";
 import { and, eq } from "drizzle-orm";
 import { images } from "./db/schema";
 import { redirect } from "next/navigation";
+import analyticsServerClient from "./analytics";
 
 export async function getMyImages() {
   const user = auth();
@@ -39,6 +40,14 @@ export async function deleteImage(id: number) {
   if(!user.userId) throw new Error("Unauthorized");
 
   await db.delete(images).where(and(eq(images.id, id), eq(images.userId, user.userId)))
+
+  analyticsServerClient.capture({
+    distinctId: user.userId,
+    event: "delete_image",
+    properties: {
+      imageId: id
+    }
+  })
 
   redirect("/");
 }
